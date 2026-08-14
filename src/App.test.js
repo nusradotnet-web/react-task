17073cae6a6076decf606de50791ae6618bf9c56
Nusra <nusradotnet@gmail.com>
2026-08-14 16:33:12 +0530

    test change change

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Modify src/App.test.js to force a failure
+test("renders learn react link", () => {
+  expect(1 + 1).toBe(3); // Intentionally failing assertion
 });
