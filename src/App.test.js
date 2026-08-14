5794cab855ca387c996998cb4359474ba6e7dbcd
Nusra <nusradotnet@gmail.com>
2026-08-14 20:36:48 +0530

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
+// Modify src/App.test.js to force a failure
 test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(1 + 1).toBe(3); // Intentionally failing assertion
 });
